c62fdab9aacfb7ac08eb2edeba776aea5c33f717
Guilherme Dantas <guidanoli@proton.me>
2024-06-12 14:42:06 -0300

    Add documentation to relay contract

--- contracts/src/VersionedBlobHashRelay.sol
+++ contracts/src/VersionedBlobHashRelay.sol
@@ -6,8 +6,18 @@ import {IInputBox} from "@cartesi/rollups/contracts/inputs/IInputBox.sol";
 import {InputRelay} from "@cartesi/rollups/contracts/inputs/InputRelay.sol";
 
 contract VersionedBlobHashRelay is InputRelay {
-    constructor(IInputBox _inputBox) InputRelay(_inputBox) {}
+    /// @notice Constructs the portal.
+    /// @param inputBox The input box used by the portal
+    constructor(IInputBox inputBox) InputRelay(inputBox) {}
 
+    /// @notice Adds an input with the versioned hashes of all the
+    /// blobs contained in the current transaction, obtained via the
+    /// `BLOBHASH` opcode (introduced by EIP-4844).
+    /// @param appContract The application contract address
+    /// @dev The input format is the following:
+    /// message sender address (20 bytes),
+    /// followed by `n` versioned blob hashes (32 bytes each),
+    /// in the same order as in the transaction.
     function relayVersionedBlobHashes(address appContract) external {
         bytes memory input = abi.encodePacked(msg.sender);
 
