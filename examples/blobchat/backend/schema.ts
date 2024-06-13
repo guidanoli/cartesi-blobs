f1c0c84791e95a55caa59a78b2e82439d6b97a62
Guilherme Dantas <guidanoli@proton.me>
2024-06-13 17:04:36 -0300

    Add prettier

--- examples/blobchat/backend/schema.ts
+++ examples/blobchat/backend/schema.ts
@@ -8,7 +8,7 @@ viem types Hex and Address instead of simple strings for some schema properties.
 */
 
 const inputFile =
-  "https://raw.githubusercontent.com/cartesi/openapi-interfaces/fce8cc7fcf2d2fcc1940e048cd16fb8550b09779/rollup.yaml";
+    "https://raw.githubusercontent.com/cartesi/openapi-interfaces/fce8cc7fcf2d2fcc1940e048cd16fb8550b09779/rollup.yaml";
 const outputFile = "src/schema.d.ts";
 
 // import types from viem in generated code
@@ -16,14 +16,17 @@ const inject = "import { Address, Hex } from 'viem';\n";
 
 console.log(`${inputFile} -> ${outputFile}`);
 openapiTS(inputFile, {
-  inject,
-  transform: (schemaObject, _options) => {
-    if ("format" in schemaObject && schemaObject.format === "hex") {
-      // use viem.Hex if format is hex
-      return schemaObject.nullable ? "Hex | null" : "Hex";
-    } else if ("format" in schemaObject && schemaObject.format === "address") {
-      // use viem.Address if format is address
-      return schemaObject.nullable ? "Address | null" : "Address";
-    }
-  },
+    inject,
+    transform: (schemaObject, _options) => {
+        if ("format" in schemaObject && schemaObject.format === "hex") {
+            // use viem.Hex if format is hex
+            return schemaObject.nullable ? "Hex | null" : "Hex";
+        } else if (
+            "format" in schemaObject &&
+            schemaObject.format === "address"
+        ) {
+            // use viem.Address if format is address
+            return schemaObject.nullable ? "Address | null" : "Address";
+        }
+    },
 }).then((output) => fs.writeFileSync(outputFile, output));
--- examples/blobchat/backend/src/index.ts
+++ examples/blobchat/backend/src/index.ts
@@ -1,5 +1,6 @@
 import createClient from "openapi-fetch";
 import { components, paths } from "./schema";
+import { Address } from "viem";
 
 type AdvanceRequestData = components["schemas"]["Advance"];
 type InspectRequestData = components["schemas"]["Inspect"];
@@ -7,47 +8,52 @@ type RequestHandlerResult = components["schemas"]["Finish"]["status"];
 type RollupsRequest = components["schemas"]["RollupRequest"];
 type InspectRequestHandler = (data: InspectRequestData) => Promise<void>;
 type AdvanceRequestHandler = (
-  data: AdvanceRequestData
+    data: AdvanceRequestData,
 ) => Promise<RequestHandlerResult>;
+
+const VERSIONED_BLOB_HASH_PORTAL: Address =
+    "0xab7528bb862fb57e8a2bcd567a2e929a0be56a5e";
 
 const rollupServer = process.env.ROLLUP_HTTP_SERVER_URL;
 console.log("HTTP rollup_server url is " + rollupServer);
 
 const handleAdvance: AdvanceRequestHandler = async (data) => {
-  console.log("Received advance request data " + JSON.stringify(data));
-  return "accept";
+    console.log("Received advance request data " + JSON.stringify(data));
+    return "accept";
 };
 
 const handleInspect: InspectRequestHandler = async (data) => {
-  console.log("Received inspect request data " + JSON.stringify(data));
+    console.log("Received inspect request data " + JSON.stringify(data));
 };
 
 const main = async () => {
-  const { POST } = createClient<paths>({ baseUrl: rollupServer });
-  let status: RequestHandlerResult = "accept";
-  while (true) {
-    const { response } = await POST("/finish", {
-      body: { status },
-      parseAs: "text",
-    });
+    const { POST } = createClient<paths>({ baseUrl: rollupServer });
+    let status: RequestHandlerResult = "accept";
+    while (true) {
+        const { response } = await POST("/finish", {
+            body: { status },
+            parseAs: "text",
+        });
 
-    if (response.status === 200) {
-      const data = (await response.json()) as RollupsRequest;
-      switch (data.request_type) {
-        case "advance_state":
-          status = await handleAdvance(data.data as AdvanceRequestData);
-          break;
-        case "inspect_state":
-          await handleInspect(data.data as InspectRequestData);
-          break;
-      }
-    } else if (response.status === 202) {
-      console.log(await response.text());
+        if (response.status === 200) {
+            const data = (await response.json()) as RollupsRequest;
+            switch (data.request_type) {
+                case "advance_state":
+                    status = await handleAdvance(
+                        data.data as AdvanceRequestData,
+                    );
+                    break;
+                case "inspect_state":
+                    await handleInspect(data.data as InspectRequestData);
+                    break;
+            }
+        } else if (response.status === 202) {
+            console.log(await response.text());
+        }
     }
-  }
 };
 
 main().catch((e) => {
-  console.log(e);
-  process.exit(1);
+    console.log(e);
+    process.exit(1);
 });
