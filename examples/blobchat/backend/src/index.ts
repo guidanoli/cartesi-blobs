962d4d04ececcb10dd077d0e82cb8de633f444ac
Guilherme Dantas <guidanoli@proton.me>
2024-06-14 11:14:16 -0300

    Query blobs through GIO requests

--- examples/blobchat/backend/src/index.ts
+++ examples/blobchat/backend/src/index.ts
@@ -2,13 +2,16 @@ import createClient from "openapi-fetch";
 import { components, paths } from "./schema";
 import { Address, Hex, slice, size, isAddressEqual } from "viem";
 
+type PostMethod = ReturnType<typeof createClient<paths>>["POST"];
 type AdvanceRequestData = components["schemas"]["Advance"];
 type InspectRequestData = components["schemas"]["Inspect"];
 type RequestHandlerResult = components["schemas"]["Finish"]["status"];
 type RollupsRequest = components["schemas"]["RollupRequest"];
+type GioResponse = components["schemas"]["GioResponseRollup"];
 type InspectRequestHandler = (data: InspectRequestData) => Promise<void>;
 type AdvanceRequestHandler = (
     data: AdvanceRequestData,
+    POST: PostMethod,
 ) => Promise<RequestHandlerResult>;
 
 const VERSIONED_BLOB_HASH_PORTAL: Address =
@@ -18,34 +21,69 @@ const rollupServer = process.env.ROLLUP_HTTP_SERVER_URL;
 console.log("HTTP rollup_server url is " + rollupServer);
 
 // Uses continuation-passing style
-const split = <T>(hex: Hex, at: number, k: (before: Hex, after: Hex) => T): T => {
+const split = <T>(
+    hex: Hex,
+    at: number,
+    k: (before: Hex, after: Hex) => T,
+): T => {
     return k(slice(hex, 0, at), at < size(hex) ? slice(hex, at) : "0x");
-}
+};
 
-const handleAdvanceFromVersionedBlobHashPortal: AdvanceRequestHandler = async ({ payload }) => {
-    const [sender, versionedBlobHashes] = split(payload, 20, (sender, payload) => {
-        const versionedBlobHashes: Hex[] = [];
-        while (size(payload) > 0) {
-            payload = split(payload, 32, (versionedBlobHash, payload) => {
-                versionedBlobHashes.push(versionedBlobHash);
-                return payload;
-            });
-        }
-        return [sender, versionedBlobHashes];
-    });
+const handleAdvanceFromVersionedBlobHashPortal: AdvanceRequestHandler = async (
+    { payload },
+    POST,
+) => {
+    const [sender, versionedBlobHashes] = split(
+        payload,
+        20,
+        (sender, payload) => {
+            const versionedBlobHashes: Hex[] = [];
+            while (size(payload) > 0) {
+                payload = split(payload, 32, (versionedBlobHash, payload) => {
+                    versionedBlobHashes.push(versionedBlobHash);
+                    return payload;
+                });
+            }
+            return [sender, versionedBlobHashes];
+        },
+    );
     console.log(`Received ${versionedBlobHashes.length} blobs from ${sender}:`);
-    versionedBlobHashes.forEach((versionedblobHash, i) => {
-        console.log();
-        console.log(`Blob at index ${i}:`)
-        console.log(versionedblobHash);
-    });
-    return "accept";
-}
+    for (const versionedBlobHash of versionedBlobHashes) {
+        console.log(`Retrieving blob from versioned hash ${versionedBlobHash}`);
+        const { response } = await POST("/gio", {
+            body: {
+                domain: 4844,
+                id: versionedBlobHash,
+            },
+            parseAs: "text",
+        });
 
-const handleAdvance: AdvanceRequestHandler = async (data) => {
+        if (response.status == 200) {
+            const data = (await response.json()) as GioResponse;
+            if (data.code == 42) {
+                const blob = data.data;
+                console.log(`Blob retrieved: ${blob}`);
+            } else {
+                console.log(
+                    `GIO request failed with code ${data.code}: ${data.data}`,
+                );
+                return "reject";
+            }
+        } else {
+            const data = await response.text();
+            console.log(
+                `HTTP request failed with status ${response.status}: ${data}`,
+            );
+            return "reject";
+        }
+    }
+    return "accept";
+};
+
+const handleAdvance: AdvanceRequestHandler = async (data, POST) => {
     console.log("Received advance request data " + JSON.stringify(data));
     if (isAddressEqual(data.metadata.msg_sender, VERSIONED_BLOB_HASH_PORTAL)) {
-        return handleAdvanceFromVersionedBlobHashPortal(data);
+        return handleAdvanceFromVersionedBlobHashPortal(data, POST);
     }
     return "accept";
 };
@@ -69,6 +107,7 @@ const main = async () => {
                 case "advance_state":
                     status = await handleAdvance(
                         data.data as AdvanceRequestData,
+                        POST,
                     );
                     break;
                 case "inspect_state":
