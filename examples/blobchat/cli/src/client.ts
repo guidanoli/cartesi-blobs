1db5f12349ef5f3ba20f6ed1e83100c550ebdbe5
Guilherme Dantas <guidanoli@proton.me>
2024-06-15 11:56:37 -0300

    Add `ACCOUNT_INDEX` env to CLI

--- examples/blobchat/cli/src/client.ts
+++ examples/blobchat/cli/src/client.ts
@@ -3,12 +3,17 @@ import { mnemonicToAccount } from "viem/accounts";
 import { sepolia } from "viem/chains";
 
 const mnemonic = process.env.MNEMONIC;
+const accountIndex = process.env.ACCOUNT_INDEX ? Number(process.env.ACCOUNT_INDEX) : undefined;
 
 if (mnemonic === undefined) {
     throw new Error("Expected environment variable MNEMONIC to be set");
 }
 
-export const account = mnemonicToAccount(mnemonic);
+if (accountIndex !== undefined && isNaN(accountIndex)) {
+    throw new Error("Environment variable ACCOUNT_INDEX must be a valid number");
+}
+
+export const account = mnemonicToAccount(mnemonic, { accountIndex });
 
 export const client = createWalletClient({
     account,
