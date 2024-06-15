52e309f3f8bb195b48a65c5e422e3e93a1d94965
Guilherme Dantas <guidanoli@proton.me>
2024-06-15 00:34:22 -0300

    Add TS CLI Front-end skeleton

---
+++ examples/blobchat/ts-cli-frontend/src/client.ts
@@ -0,0 +1,17 @@
+import { createWalletClient, http } from 'viem'
+import { mnemonicToAccount } from 'viem/accounts'
+import { sepolia } from 'viem/chains'
+
+const mnemonic = process.env.MNEMONIC;
+
+if (mnemonic === undefined) {
+    throw new Error("Expected environment variable MNEMONIC to be set");
+}
+
+export const account = mnemonicToAccount(mnemonic)
+ 
+export const client = createWalletClient({
+  account,
+  chain: sepolia,
+  transport: http(),
+})
