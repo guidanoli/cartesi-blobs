f4ac1c6fa6e32fbec3b2eee72e6956aa9908a782
Guilherme Dantas <guidanoli@proton.me>
2024-06-15 12:10:19 -0300

    Add `PRIVATE_KEY` to CLI

--- examples/blobchat/cli/src/client.ts
+++ examples/blobchat/cli/src/client.ts
@@ -1,19 +1,27 @@
-import { createWalletClient, http } from "viem";
-import { mnemonicToAccount } from "viem/accounts";
+import { createWalletClient, http, isHex } from "viem";
+import { privateKeyToAccount, mnemonicToAccount } from "viem/accounts";
 import { sepolia } from "viem/chains";
 
-const mnemonic = process.env.MNEMONIC;
-const accountIndex = process.env.ACCOUNT_INDEX ? Number(process.env.ACCOUNT_INDEX) : undefined;
-
-if (mnemonic === undefined) {
-    throw new Error("Expected environment variable MNEMONIC to be set");
+function getAccount() {
+    const mnemonic = process.env.MNEMONIC;
+    const privateKey = process.env.PRIVATE_KEY;
+    if (mnemonic !== undefined) {
+        const accountIndex = process.env.ACCOUNT_INDEX ? Number(process.env.ACCOUNT_INDEX) : undefined;
+        if (accountIndex !== undefined && isNaN(accountIndex)) {
+            throw new Error("Environment variable ACCOUNT_INDEX must be a valid number");
+        }
+        return mnemonicToAccount(mnemonic, { accountIndex });
+    } else if (privateKey !== undefined) {
+        if (!isHex(privateKey)) {
+            throw new Error("Environment variable PRIVATE_KEY must be in hexstring format");
+        }
+        return privateKeyToAccount(privateKey);
+    } else {
+        throw new Error("Expected either environment variables MNEMONIC or PRIVATE_KEY to be set");
+    }
 }
 
-if (accountIndex !== undefined && isNaN(accountIndex)) {
-    throw new Error("Environment variable ACCOUNT_INDEX must be a valid number");
-}
-
-export const account = mnemonicToAccount(mnemonic, { accountIndex });
+export const account = getAccount();
 
 export const client = createWalletClient({
     account,
